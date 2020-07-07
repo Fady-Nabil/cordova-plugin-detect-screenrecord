22f1b416515190e38bad737cca122d745b2d4187
Ebrahim Maher <44779868+ebrahimMaher@users.noreply.github.com>
2020-07-07 10:04:48 +0200

    fix { bug

--- src/ios/detect-screenshot.m
+++ src/ios/detect-screenshot.m
@@ -6,29 +6,28 @@
     @implementation screenshot
     - (void)pluginInitialize
     {
-            BOOL isCaptured = [[UIScreen mainScreen] isCaptured];
-            if(isCaptured){
-                if ([self.webView respondsToSelector:@selector(stringByEvaluatingJavaScriptFromString:)]) {
-                    // UIWebView
-                    [self.webView performSelectorOnMainThread:@selector(stringByEvaluatingJavaScriptFromString:) withObject:@"cordova.fireDocumentEvent('screenrecordstart');" waitUntilDone:NO];
-                } else if ([self.webView respondsToSelector:@selector(evaluateJavaScript:completionHandler:)]) {
-                    // WKWebView
-                    [self.webView performSelector:@selector(evaluateJavaScript:completionHandler:) withObject:@"cordova.fireDocumentEvent('screenrecordstart');" withObject:nil];
-                } else {
-                    // cordova lib version is > 4
-                    [self.commandDelegate evalJs:@"cordova.fireDocumentEvent('screenrecordstart');" ];
-                }
-            }else{
-                if ([self.webView respondsToSelector:@selector(stringByEvaluatingJavaScriptFromString:)]) {
-                    // UIWebView
-                    [self.webView performSelectorOnMainThread:@selector(stringByEvaluatingJavaScriptFromString:) withObject:@"cordova.fireDocumentEvent('screenrecordstop');" waitUntilDone:NO];
-                } else if ([self.webView respondsToSelector:@selector(evaluateJavaScript:completionHandler:)]) {
-                    // WKWebView
-                    [self.webView performSelector:@selector(evaluateJavaScript:completionHandler:) withObject:@"cordova.fireDocumentEvent('screenrecordstop');" withObject:nil];
-                } else {
-                    // cordova lib version is > 4
-                    [self.commandDelegate evalJs:@"cordova.fireDocumentEvent('screenrecordstop');" ];
-                }
+        BOOL isCaptured = [[UIScreen mainScreen] isCaptured];
+        if(isCaptured){
+            if ([self.webView respondsToSelector:@selector(stringByEvaluatingJavaScriptFromString:)]) {
+                // UIWebView
+                [self.webView performSelectorOnMainThread:@selector(stringByEvaluatingJavaScriptFromString:) withObject:@"cordova.fireDocumentEvent('screenrecordstart');" waitUntilDone:NO];
+            } else if ([self.webView respondsToSelector:@selector(evaluateJavaScript:completionHandler:)]) {
+                // WKWebView
+                [self.webView performSelector:@selector(evaluateJavaScript:completionHandler:) withObject:@"cordova.fireDocumentEvent('screenrecordstart');" withObject:nil];
+            } else {
+                // cordova lib version is > 4
+                [self.commandDelegate evalJs:@"cordova.fireDocumentEvent('screenrecordstart');" ];
+            }
+        }else{
+            if ([self.webView respondsToSelector:@selector(stringByEvaluatingJavaScriptFromString:)]) {
+                // UIWebView
+                [self.webView performSelectorOnMainThread:@selector(stringByEvaluatingJavaScriptFromString:) withObject:@"cordova.fireDocumentEvent('screenrecordstop');" waitUntilDone:NO];
+            } else if ([self.webView respondsToSelector:@selector(evaluateJavaScript:completionHandler:)]) {
+                // WKWebView
+                [self.webView performSelector:@selector(evaluateJavaScript:completionHandler:) withObject:@"cordova.fireDocumentEvent('screenrecordstop');" withObject:nil];
+            } else {
+                // cordova lib version is > 4
+                [self.commandDelegate evalJs:@"cordova.fireDocumentEvent('screenrecordstop');" ];
             }
         }
        
