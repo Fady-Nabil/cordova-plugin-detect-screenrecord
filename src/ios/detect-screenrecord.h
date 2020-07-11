b4d32a7be5678a395d8a3e4b4c022e4f589f65f4
Ebrahim Maher <hema.developer.74@gmail.com>
2020-07-11 16:16:58 +0200

    update plugin

---
+++ src/ios/detect-screenrecord.h
@@ -0,0 +1,19 @@
+/********* detect-screenrecord.h Cordova Plugin Header *******/
+
+#import <Cordova/CDV.h>
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+
+extern NSString *kScreenRecordingDetectorRecordingStatusChangedNotification;
+
+@interface detectscreenrecord : CDVPlugin
+
+- (void)detectscreenrecord:(CDVInvokedUrlCommand*)command;
+
++(instancetype)sharedInstance;
++ (void)triggerDetectorTimer;
++ (void)stopDetectorTimer;
+- (BOOL)isRecording;
+
+@end
